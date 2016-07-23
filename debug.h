0d37f965cf180d9f82cdf64d72bcb4fb0168da44
DUVILLIE Guillerme <gduvillie@openmailbox.org>
2016-07-23 10:45:13 +0200

    More complete debug macros

--- debug.h
+++ debug.h
@@ -30,10 +30,23 @@
     fprintf(stderr,##__VA_ARGS__);\
 }while(0);
 
+#define OL_DEB_PRINT(...) do {\
+    fprintf(stderr,##__VA_ARGS__);\
+}while(0);
+
 #define PRINTVAR(x) fprintf(stderr, "DEBUG -- %s:%d " #x " = %d\n", __FILE__, __LINE__, x);
+#define OL_PRINTVAR(x) fprintf(stderr, "%s:%d " #x " = %d\n", __FILE__, __LINE__, x);
+
+#define PRINTSTR(x) fprintf(stderr, "DEBUG -- %s:%d " #x " = %s\n", __FILE__, __LINE__, x);
+#define OL_PRINTSTR(x) fprintf(stderr, "%s:%d " #x " = %s\n", __FILE__, __LINE__, x);
+
 #else
 #define DEB_PRINT(...)
 #define PRINTVAR(x)
+#define PRINTSTR(x)
+#define OL_DEB_PRINT(...)
+#define OL_PRINTVAR(x)
+#define OL_PRINTSTR(x)
 #endif
 
 #endif
